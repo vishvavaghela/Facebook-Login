2e23359113576970dd4cd1ad3ab00206e5e32bff
hp <ftvishvavaghela03@gmail.com>
2022-09-06 15:57:06 +0530

    test123

--- app/src/main/java/com/example/facebooksociallogin/MainActivity.java
+++ app/src/main/java/com/example/facebooksociallogin/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException exception) {
-                //App Code
+                //App Code123
             }
         });
     }
